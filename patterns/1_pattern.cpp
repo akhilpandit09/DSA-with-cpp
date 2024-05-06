4c765afc97934a55f40d4a01d78ba62408b882a6
akhilpandit09 <akhileshwarpandit0024@gmail.com>
2024-05-07 00:00:14 +0545

    Added pattern problems solving code

---
+++ patterns/1_pattern.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <=5; i++)
+    {
+        for (int j = 1; j<=5 ; j++)
+        {
+            cout<<"*"<<" ";
+
+        }
+        cout<<endl;
+        
+    }
+    
+}
