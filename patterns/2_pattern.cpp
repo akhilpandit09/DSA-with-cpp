8c86c98e32d8279a98746c1c1dd66dc8e5d6d3cb
akhilpandit09 <akhileshwarpandit0024@gmail.com>
2024-05-07 00:25:16 +0545

    Added second pattern problem code

---
+++ patterns/2_pattern.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int row=1; row<=4; row++){
+        for(int col=1; col<=5; col++){
+            cout<<row<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+
