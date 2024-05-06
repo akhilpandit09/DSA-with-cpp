e239e67b869a18a5039c7e414a2e909b9ce1b8a2
akhilpandit09 <akhileshwarpandit0024@gmail.com>
2024-05-07 00:38:46 +0545

    Added pattern problem solution

---
+++ patterns/4_pattern.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 5; col >= 1; col--)
+        {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// pattern Que 3
+
+// 5 4 3 2 1
+// 5 4 3 2 1
+// 5 4 3 2 1
+// 5 4 3 2 1
+// 5 4 3 2 1
