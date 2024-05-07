ea4440447df3d417794f05e5431c70c0afc6d4d8
akhilpandit09 <akhileshwarpandit0024@gmail.com>
2024-05-07 15:17:04 +0545

    Added pattern problem solution

---
+++ patterns/9_pattern.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // first approach
+
+    for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 1; col <= 5; col++)
+        {
+            char name = 'a' + col - 1;
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+
+    cout<<"---------------------------------------"<<endl;
+
+    // second approach
+
+    for(int row = 1; row <=5; row++){
+        for (char col = 'a'; col <= 'e'; col++)
+        {
+            cout << col << " ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// pattern Que 9
+
+// a b c d e
+// a b c d e
+// a b c d e
+// a b c d e
+// a b c d e
