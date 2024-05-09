e9f831062311ccfa9a79095047eacf83f2df4e42
akhilpandit09 <akhileshwarpandit0024@gmail.com>
2024-05-10 00:28:58 +0545

    modify the solution

--- patterns/31_pattern.cpp
+++ patterns/31_pattern.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main()
 {
-    int row, col;
-    for (row =5; row >= 1; row--)
+    int row, col, n;
+    cout << "Enter a number:";
+    cin >> n;
+    for (row =n; row >= 1; row--)
     {
         for (col = 1; col <= row-1; col++)
         {
             cout << " ";
         }
-        for (col = 5; col >= row; col--)
+        for (col = n; col >= row; col--)
         {
             cout << col;
         }
