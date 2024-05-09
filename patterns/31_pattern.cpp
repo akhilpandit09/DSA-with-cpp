35e7fb09b026dc08c1bdede5b35b30758ba663c8
akhilpandit09 <akhileshwarpandit0024@gmail.com>
2024-05-10 00:26:38 +0545

    Added hard pattern problem solution

---
+++ patterns/31_pattern.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    for (row =5; row >= 1; row--)
+    {
+        for (col = 1; col <= row-1; col++)
+        {
+            cout << " ";
+        }
+        for (col = 5; col >= row; col--)
+        {
+            cout << col;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+//              5
+//            5 4
+//          5 4 3
+//        5 4 3 2 
+//      5 4 3 2 1
